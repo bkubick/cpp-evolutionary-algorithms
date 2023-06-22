cbd97f1c8eee4ac35c53af855660f8e6ccbca087
bkubick <bjk2158@columbia.edu>
2023-06-21 20:09:26 -0500

    Update main to run and plot various values

--- src/traveling_salesman/ga/main.cpp
+++ src/traveling_salesman/ga/main.cpp
@@ -1,8 +1,10 @@
 #include <fstream>
 #include <iostream>
+#include <limits>
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <tuple>
 #include <vector>
 
 #include "matplotlibcpp.h"
@@ -58,30 +60,25 @@ std::vector< std::vector<double> > readCoordinateCsvFile(const std::string& file
 
 
 /**
- * Helper used to output a plot of the lowest total distance (best fitness) for each generation
- * to the corresponding pdf file name.
+ * Helper to round double to set number of decimal places within a string.
  * 
- * @param pdf_file_name         the name of the output pdf file with the plot.
- * @param fitnesses             the list of lowest fitnesses for each generation.
- * @param mutation_probability  the probability of the mutation used for the algorithm.
+ * @param value         the double to be rounded.
+ * @param precisiion    how many decimal places to round the double to.
+ * @return the string of the round double.
 */
-void saveFitnessPlot(std::string pdf_file_name, std::vector<double>& fitnesses, double const& mutation_probability)
-{
-    std::vector<int> generations(fitnesses.size());
-
-    plt::plot(fitnesses);
-
-    plt::title("Total Distance Traveled");
-    plt::xlabel("Generations \n (Mutation Probability - ) " + std::to_string(mutation_probability));
-    plt::ylabel("Total Distance");
-    plt::tight_layout();
-
-    plt::savefig(pdf_file_name);
+std::string roundDouble(double value, int precision = 3) {
+    std::ostringstream ss;
+    ss << std::fixed << std::setprecision(precision) << value;
+    return ss.str();
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc == 1) {
+        throw std::runtime_error("Filename argument for plot file not passed in");
+    }
+
     std::cout << "Starting Main" << std::endl;
     std::string test_file = "./data/traveling_salesman/TSP1.csv";
 
@@ -94,16 +91,48 @@ int main()
     Fitness fitness = Fitness(coordinates);
     GeneticAlgorithm ga = GeneticAlgorithm(fitness);
 
-    // Running algorithm
-    const std::string figure_file = "test.pdf";
-    const double mutation_probability = 0.01;
-    const int number_of_generations = 50;
-    const int population_size = 30;
+    // Running algorithm (population_size, number_of_generations, mutation_probability)
+    // Note that the limit is based off total individuals of 100,000
+    std::vector< std::tuple<int, int, double> > algorithm_constants = {
+        {10, 10000, 0.001},
+        {20, 5000, 0.001},
+        {50, 2000, 0.001},
+        {10, 10000, 0.01},
+        {20, 5000, 0.01},
+        {50, 2000, 0.01},
+        {10, 10000, 0.05},
+        {20, 5000, 0.05},
+        {50, 2000, 0.05},
+    };
 
-    std::cout << "Starting Genetic algorithm" << std::endl;
-    std::vector<double> fitnesses = ga.run(population_size, number_of_generations, mutation_probability);
+    // Output variables
+    const std::string figure_file = argv[1];
 
-    saveFitnessPlot(figure_file, fitnesses, mutation_probability);
+    for (auto algorithm_constant : algorithm_constants) {
+        const double mutation_probability = std::get<2>(algorithm_constant);
+        const int number_of_generations = std::get<1>(algorithm_constant);
+        const int population_size = std::get<0>(algorithm_constant);
+
+        std::string output_title = "Running Genetic Algorithm";
+        std::string population_text = "Population: " + std::to_string(population_size);
+        std::string generation_text = "Generations: " + std::to_string(number_of_generations);
+        std::string mutation_text = "Mutation Prob: " + roundDouble(mutation_probability);
+
+        std::cout << output_title << " - " << population_text << " " << generation_text << " " << mutation_text << std::endl;
+
+        std::vector<double> fitnesses = ga.run(population_size, number_of_generations, mutation_probability);
+
+        std::string label = population_text + " " + generation_text + " " + mutation_text;
+
+        plt::plot(fitnesses, {{"label", label}});
+        plt::title("Total Distance Traveled");
+        plt::xlabel("Generations");
+        plt::ylabel("Total Distance");
+        plt::legend();
+        plt::tight_layout();
+    }
+
+    plt::savefig(figure_file);
 
     return 0;
 }
